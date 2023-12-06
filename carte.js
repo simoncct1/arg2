e5a9f3131cfa5d430e7aa15e85a6984d4b45e665
simoncct1 <simoncouclet@gmail.com>
2023-12-07 00:57:44 +0100

    debutmapr

--- carte.js
+++ carte.js
@@ -2,4 +2,74 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { InteractionManager } from '/node_modules/three.interactive/build/three.interactive.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+document.querySelector(".enigme").addEventListener('click', function (e) {
+   
+    // create a scene, that will hold all our elements such as objects, cameras and lights.
+    const scene = new THREE.Scene();
+    // create a camera, which defines where we're looking at
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+    // tell the camera where to look
+    camera.position.set(0, 17, 0);
 
+    const renderer = new THREE.WebGLRenderer();
+    var container = document.querySelector(".map");
+    console.log(container.offsetWidth)
+    renderer.setSize(container.offsetWidth, container.offsetHeight);
+    // add the output of the render function to the HTML
+    document.querySelector(".enigme").appendChild(renderer.domElement);
+    const loader = new GLTFLoader();
+    var map= new THREE.Object3D();
+ loader.load("/assets/map.glb",
+     function ( gltf ) {
+         map = gltf.scene
+         gltf.scene.scale.set(0.1,0.1,0.1);
+         scene.add( gltf.scene );
+     },
+     function onError(err) {
+         console.log('An error happened');
+     }
+    );
+
+   const light = new THREE.PointLight( 0xffffff, 3, 800 );
+   light.position.set( 0, 10, 0);
+   scene.add( light );
+   const light4 = new THREE.PointLight( 0xDC143C, 1.55, 800 );
+   light4.position.set( 0, 10, 0);
+   scene.add( light4 );
+   const light2 = new THREE.PointLight( 0xDC143C, 3, 800 );
+   light2.position.set( -20, 10, 0);
+   scene.add( light2 );
+   const light3 = new THREE.PointLight( 0xDC143C, 3, 800);
+   light3.position.set( 20, 10, 0);
+   scene.add( light3 );
+   const interactionManager = new InteractionManager(
+    renderer,
+    camera,
+    renderer.domElement
+  );
+
+  
+//control
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.mouseButtons = {
+	LEFT: THREE.MOUSE.PAN,
+	MIDDLE: THREE.MOUSE.DOLLY,
+	RIGHT: THREE.MOUSE.ROTATE
+}
+
+controls.update();
+
+    // function for re-rendering/animating the scene
+    function tick() {
+        requestAnimationFrame(tick);
+        renderer.render(scene, camera);
+    }
+    tick();
+},{once : true})
+document.querySelector(".enigme").addEventListener('click', function (e) {
+document.querySelector(".mapicon").style.display ="none";
+})
+document.querySelector("#blocker").addEventListener('click', function (e) {
+    document.querySelector(".mapicon").style.display ="block";
+})
