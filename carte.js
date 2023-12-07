d67efa8b93e7203c3b42d40d8045fc23d8cb13d6
simoncct1 <simoncouclet@gmail.com>
2023-12-07 03:14:19 +0100

    hdr

--- carte.js
+++ carte.js
@@ -3,6 +3,8 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { InteractionManager } from '/node_modules/three.interactive/build/three.interactive.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+import {PMREMGenerator} from "/node_modules/three/src/extras/PMREMGenerator.js";
+
 document.querySelector(".enigme").addEventListener('click', function (e) {
    
     // create a scene, that will hold all our elements such as objects, cameras and lights.
@@ -19,18 +21,31 @@ document.querySelector(".enigme").addEventListener('click', function (e) {
     // add the output of the render function to the HTML
     document.querySelector(".enigme").appendChild(renderer.domElement);
     const loader = new GLTFLoader();
+    const pmremGenerator = new PMREMGenerator();
     var map= new THREE.Object3D();
+    var flagis;
  loader.load("/assets/map.glb",
      function ( gltf ) {
          map = gltf.scene
          gltf.scene.scale.set(0.1,0.1,0.1);
          scene.add( gltf.scene );
+         flagis = scene.children[4].children[4];
+         interactionManager.add(flagis);
+         flagis.addEventListener('click', (event) => {new RGBELoader()
+            .load('/assets/pal.hdr', (hdrEquiRect, textureData) => {
+                    hdrCubeRenderTarget = pmremGenerator.fromEquirectangular(hdrEquiRect);
+                    pmremGenerator.compileCubemapShader();
+            
+                    scene.background = hdrCubeRenderTarget.texture;
+                    renderer.toneMapping = LinearToneMapping;
+                    renderer.toneMappingExposure = 0.5;
+            });});
      },
      function onError(err) {
          console.log('An error happened');
      }
     );
-
+    console.log(flagis)
    const light = new THREE.PointLight( 0xffffff, 3, 800 );
    light.position.set( 0, 10, 0);
    scene.add( light );
@@ -50,12 +65,14 @@ document.querySelector(".enigme").addEventListener('click', function (e) {
   );
 
   
+ 
 //control
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.mouseButtons = {
 	LEFT: THREE.MOUSE.PAN,
 	MIDDLE: THREE.MOUSE.DOLLY,
 	RIGHT: THREE.MOUSE.ROTATE
+
 }
 
 controls.update();
